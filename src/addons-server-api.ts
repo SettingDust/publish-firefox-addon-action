b1556b3cec27478b3c086b5b7de04afed63b1d9e
SettingDust <settingdust@gmail.com>
2022-04-02 13:24:47 +0800

    fix: wrong api url

--- src/addons-server-api.ts
+++ src/addons-server-api.ts
@@ -20,7 +20,7 @@ export function upload(addonId: string, addon: PathLike, source?: PathLike) {
   const form = new FormData()
   form.append('upload', createReadStream(addon))
   if (source) form.append('source', createReadStream(source))
-  return got(`${API_BASE}/${encodeURIComponent(addonId)}/versions/`, {
+  return got(`${API_BASE}/addon/${encodeURIComponent(addonId)}/versions/`, {
     method: 'post',
     responseType: 'json',
     body: form
